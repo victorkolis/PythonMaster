bf6cf3ab7d6b01137b04480d330e064a63746c50
Victor deMatos <58838266+victorkolis@users.noreply.github.com>
2020-11-20 18:28:50 -0300

    Update variables.py

--- OneHundredDays/Day01/variables.py
+++ OneHundredDays/Day01/variables.py
@@ -1,3 +1,5 @@
+# This software objective is to show how variables operate(in a very basic way) and show how they facilitate/are necessary for the programming process
+
 #01 - simple use of a variable to store the input collected from user input
 name = input("What is your name? ")
 print(name)
