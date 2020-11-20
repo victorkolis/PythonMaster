84beaac686376366ea76d21cb66dd630fbb5ee21
Victor Kolis <>
2020-11-20 18:20:29 -0300

    20201120

---
+++ OneHundredDays/Day01/final_challenge_band_name_generator.py
@@ -0,0 +1,5 @@
+# This software objective is to combine two names into one to generate a band name
+print("Howdy! Welcome to the band name generator")
+city_user_grew_up_in = input("What city did your grow up in?\n")
+user_pet_name = input("What is the name of your pet?\n")
+print("The name of the band could be " + city_user_grew_up_in + " " + user_pet_name)
