078ed6ed998e7f9fd3e5144fcafdee4adb14ff9c
Victor Kolis <>
2020-11-20 18:05:25 -0300

    20201120

---
+++ OneHundredDays/Day01/challenge1_4.py
@@ -0,0 +1,12 @@
+a = input("a: ")
+b = input("b: ")
+
+a,b = b,a
+
+# another possible way
+# c = a
+# a = b
+# b = c 
+
+print("a: " + a)
+print("b: " + b)
---
+++ OneHundredDays/Day01/len_function.py
@@ -0,0 +1,2 @@
+# This piece of software objective is to print the lenght of one's name
+print(len(input("What is your name? ")))
