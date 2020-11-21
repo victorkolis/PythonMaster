aedb16f95776712271f96678ea7e7e2652272fcd
Victor Kolis <victorkolis@protonmail.com>
2020-11-21 09:36:47 -0300

    20201121

---
+++ OneHundredDays/Day02/casting.py
@@ -0,0 +1,8 @@
+# Converting data types into another
+# Strings and booleans cannot perform the arithmetic operation
+
+print("You are " + str(26) + " years old.")
+
+favorite_number = input("What is your favorite number? ")
+new_favorite_number = str(favorite_number)
+print("My favorite number is also " + new_favorite_number + ".")
