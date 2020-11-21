34ab492be84d97a3c25f05ee95afe78a6349e191
Victor Kolis <victorkolis@protonmail.com>
2020-11-21 09:40:09 -0300

    20201121

--- OneHundredDays/Day02/casting.py
+++ OneHundredDays/Day02/casting.py
@@ -6,3 +6,8 @@ print("You are " + str(26) + " years old.")
 favorite_number = input("What is your favorite number? ")
 new_favorite_number = str(favorite_number)
 print("My favorite number is also " + new_favorite_number + ".")
+
+float(20)
+int("33")
+str(2)
+bool("True")
