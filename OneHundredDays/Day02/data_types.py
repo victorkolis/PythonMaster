e8b362d84ebbff3ea03d8833f138267f81d49d1b
Victor Kolis <victorkolis@protonmail.com>
2020-11-21 09:26:16 -0300

    20201121

---
+++ OneHundredDays/Day02/data_types.py
@@ -0,0 +1,17 @@
+#Data Types
+
+#String
+print("Hello"[0])
+print("Hello"[4])
+print("123" + "345")
+
+# Integer
+print(123 + 345)
+123_456_789 # Using the underscore to separate large numbers
+
+# Float
+3.141593
+
+#Boolean
+True
+False
---
+++ OneHundredDays/Day02/type_function.py
@@ -0,0 +1,5 @@
+# The type() function gives you the type of data you're working with
+
+print(type(2))
+print(type(""))
+print(type(type))
