9341bd20a15068c005113acfe266cdf9f58c26bf
Victor Kolis <victorkolis@protonmail.com>
2020-11-21 10:53:21 -0300

    20201121

---
+++ OneHundredDays/Day02/BMI_challange.py
@@ -0,0 +1,6 @@
+# This software objective is to calculate the BMIs
+
+height = float(input("Enter your height: "))
+weight = float(input("Enter your weight: "))
+
+print("Your BMI is " + str(int(weight/height ** 2)))
