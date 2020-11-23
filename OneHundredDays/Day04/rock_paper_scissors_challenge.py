de4e20c1e0058437789c6283a696d7529166834e
Victor Kolis <victorkolis@protonmail.com>
2020-11-23 19:28:08 -0300

    20201123

--- OneHundredDays/Day04/rock_paper_scissors_challenge.py
+++ OneHundredDays/Day04/rock_paper_scissors_challenge.py
@@ -29,56 +29,63 @@ scissors = '''
 ---.__(___)
 '''
 
-player_choice = int(input("What do you choose? Rock [0], Paper[1], Scissors[2]\n"))
+try:
+    player_choice = int(input("What do you choose? Rock [0], Paper[1], Scissors[2]\n"))
+except:
+    print("Error: 404 - Choose a number from 0-2!")
+
 machine_choice = random.randint(0, 2)
 rock_paper_scissors_ascii = [rock, paper, scissors]
 rps_list = ["R", "P", "S"]
 
+try:
+    if player_choice == machine_choice:
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("Draw!")
+        
+    # player wins 01
+    elif rps_list[player_choice] == "R" and rps_list[machine_choice] == "S":
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Win!")
 
-if player_choice == machine_choice:
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("Draw!")
-    
-# player wins 01
-elif rps_list[player_choice] == "R" and rps_list[machine_choice] == "S":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Win!")
+    # player wins 02
+    elif rps_list[player_choice] == "S" and rps_list[machine_choice] == "P":
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Win!")
+        
+    # player wins 03
+    elif rps_list[player_choice] == "P" and rps_list[machine_choice] == "R":
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Win!")
+        
+    # machine wins 01
+    elif rps_list[machine_choice] == "R" and rps_list[player_choice] == "S":
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Lose!")
 
-# player wins 02
-elif rps_list[player_choice] == "S" and rps_list[machine_choice] == "P":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Win!")
-    
-# player wins 03
-elif rps_list[player_choice] == "P" and rps_list[machine_choice] == "R":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Win!")
-    
-# machine wins 01
-elif rps_list[machine_choice] == "R" and rps_list[player_choice] == "S":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Lose!")
+    # machine wins 02
+    elif rps_list[machine_choice] == "S" and rps_list[player_choice] == "P":
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Lose!")
+        
+    # machine wins 03
+    else:
+        print(rock_paper_scissors_ascii[player_choice])
+        print("Computer chose:")
+        print(rock_paper_scissors_ascii[machine_choice])
+        print("You Lose!")
 
-# machine wins 02
-elif rps_list[machine_choice] == "S" and rps_list[player_choice] == "P":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Lose!")
-    
-# machine wins 03
-elif rps_list[machine_choice] == "P" and rps_list[player_choice] == "R":
-    print(rock_paper_scissors_ascii[player_choice])
-    print("Computer chose:")
-    print(rock_paper_scissors_ascii[machine_choice])
-    print("You Lose!")
+except:
+    print("That is an invalid request!")
