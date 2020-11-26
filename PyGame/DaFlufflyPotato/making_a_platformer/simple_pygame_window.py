17d7b92efb0e5ba1fe25a92f1edb00da4ef03472
Victor Kolis <victorkolis@protonmail.com>
2020-11-25 23:35:17 -0300

    20201125

--- PyGame/DaFlufflyPotato/making_a_platformer/simple_pygame_window.py
+++ PyGame/DaFlufflyPotato/making_a_platformer/simple_pygame_window.py
@@ -7,18 +7,17 @@ clock = pygame.time.Clock()
 from pygame.locals import *
 pygame.init()
 
-pygame.display.set_caption('My Pygame Window')
+pygame.display.set_caption("My Pygame Window")
 
 WINDOW_SIZE = (400, 400)
 
+# create the display
 screen = pygame.display.set_mode(WINDOW_SIZE, 0, 32)
 
-while True:
-    
+while True: 
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
     clock.tick(60) # Controls fps
-
