acfebc8762581b4f321a4fa3d7dddb362671675f
Victor Kolis <victorkolis@protonmail.com>
2020-12-05 16:25:51 -0300

    20201204

--- OneHundredDays/Day15/the_coffee_machine.py
+++ OneHundredDays/Day15/the_coffee_machine.py
@@ -1,1 +1,8 @@
-order = input('What would you like? (espresso/latte/cappuccino):')
+import items
+
+while True:
+    order = input('What would you like? (espresso/latte/cappuccino): ')
+    if order == 'off':
+        break
+    elif order == 'report':
+        print(items.MENU)
