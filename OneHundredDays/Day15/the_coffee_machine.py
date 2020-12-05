ac044131a2cc00c33310831365db1e228768aa0c
Victor Kolis <victorkolis@protonmail.com>
2020-12-05 17:25:50 -0300

    20201205

--- OneHundredDays/Day15/the_coffee_machine.py
+++ OneHundredDays/Day15/the_coffee_machine.py
@@ -2,11 +2,12 @@ import items
 
 while True:
     order = input('What would you like? (espresso/latte/cappuccino): ')
+    # Turns the coffee machine off
     if order == 'off':
         break
+    # Check the coffee machine resources
     elif order == 'report':
         resources = list(items.resources.items())
-
         for resource in resources:
             resource_name, resource_quantity = resource
             if resource_name == 'coffee':
@@ -14,4 +15,22 @@ while True:
             else:
                 print(f'{resource_name.title()}: {resource_quantity}ml ')
     elif order == 'espresso':
-        if items.resources['water'] < items.resources['water']:
+        if items.resources['water'] < items.MENU.get("espresso").get("ingredients").get("water"):
+            print(f"Sorry, there is not enough Water.")
+        elif items.resources['coffee'] < items.MENU.get("espresso").get("ingredients").get("coffee"):
+            print(f"Sorry, there is not enough Coffee.")
+
+    elif order == 'latte' or order == 'cappuccino':
+        if items.resources['water'] < items.MENU.get(order).get("ingredients").get("water"):
+            print(f"Sorry, there is not enough Water.")
+        elif items.resources['coffee'] < items.MENU.get(order).get("ingredients").get("coffee"):
+            print(f"Sorry, there is not enough Coffee.")
+        elif items.resources['milk'] < items.MENU.get(order).get("ingredients").get("milk"):
+            print(f"Sorry, there is not enough Milk.")
+
+    # Process coins
+    print('Please insert coins.')
+    quarters = int(input('QUARTERS: ')) * 0.25
+    dimes = int(input('DIMES: ')) * 0.10
+    nickles = int(input('NICKLES: ')) * 0.05
+    pennies = int(input('PENNIES: ')) * 0.01
