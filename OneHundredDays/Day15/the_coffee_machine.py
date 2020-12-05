e26d096d90a6c2a2078ad4d9a3a1c95a516d9688
Victor Kolis <victorkolis@protonmail.com>
2020-12-05 19:32:27 -0300

    20201205

--- OneHundredDays/Day15/the_coffee_machine.py
+++ OneHundredDays/Day15/the_coffee_machine.py
@@ -12,6 +12,8 @@ while True:
             resource_name, resource_quantity = resource
             if resource_name == 'coffee':
                 print(f'{resource_name.title()}: {resource_quantity}g ')
+            elif resource_name == 'money':
+                print(f'{resource_name.title()}: ${resource_quantity} ')
             else:
                 print(f'{resource_name.title()}: {resource_quantity}ml ')
     elif order == 'espresso':
@@ -28,9 +30,23 @@ while True:
         elif items.resources['milk'] < items.MENU.get(order).get("ingredients").get("milk"):
             print(f"Sorry, there is not enough Milk.")
 
-    # Process coins
-    print('Please insert coins.')
-    quarters = int(input('QUARTERS: ')) * 0.25
-    dimes = int(input('DIMES: ')) * 0.10
-    nickles = int(input('NICKLES: ')) * 0.05
-    pennies = int(input('PENNIES: ')) * 0.01
+    if order == 'espresso' or order == 'latte' or order == 'cappuccino':
+        # Process coins
+        print('Please insert coins.')
+        quarters = float(input('QUARTERS: ')) * 0.25
+        dimes = float(input('DIMES: ')) * 0.10
+        nickles = float(input('NICKLES: ')) * 0.05
+        pennies = float(input('PENNIES: ')) * 0.01
+        coins_total = round(quarters + dimes + nickles + pennies, 2)
+
+        # Check the amount of money summed up
+        if coins_total > items.MENU[order]['cost']:
+            print(f"Here is ${coins_total - items.MENU[order]['cost']} in change")
+            print(f"Here is your {order} ☕️ enjoy!!")
+
+            # Subtract the resources in order to emulate a real coffee machine using its internal resources.
+            for resource in items.MENU.get('espresso').values():
+                item = resource
+                print(f"{item['water']}")
+        else:
+            print("Sorry, that's not enough money. Money refunded.")
