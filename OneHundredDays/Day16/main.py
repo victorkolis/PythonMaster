1e69beaa69766af88863e2ba604e1924816e116a
Victor Kolis <victorkolis@protonmail.com>
2020-12-06 10:24:06 -0300

    20201206

--- OneHundredDays/Day16/main.py
+++ OneHundredDays/Day16/main.py
@@ -1,27 +1,4 @@
-# import my_module
-# from turtle import Turtle, Screen
-#
-#
-# print(my_module.variable)
-#
-# # Constructing an object
-# bert = Turtle()
-#
-# # Methods
-# bert.shape('turtle')
-# bert.color('chocolate4')
-# bert.forward(100)
-#
-# my_screen = Screen()
-# print(my_screen.canvheight)
-# my_screen.exitonclick()
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-from prettytable import PrettyTable
-
-table = PrettyTable()
-
-
-table.add_column('Names', ['Victor', 'Vinicius', 'Isaac'])
-table.add_column('Age', ['26', '26', '2'])
-table.align = 'r'
-print(table)
