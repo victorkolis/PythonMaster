f2128a5587d4ecc419abf15d64a05de096764c39
Victor Kolis <victorkolis@protonmail.com>
2020-12-07 22:40:29 -0300

    20201207

--- OneHundredDays/Review01/tip_calculator_discount.py
+++ OneHundredDays/Review01/tip_calculator_discount.py
@@ -4,20 +4,37 @@
 import subprocess
 
 def tip_calculator():
-    suprocess.call('clear')
+    subprocess.call('clear')
     print('Welcome to the tip calculator.')
-    bill_total = int(input('The bill total is: $'))
-    if bill_total > 300 or str(bill_total).endswith('7'):
-        bill_total -= bill_total * 0.03
-    
+    try:
+        bill_total = float(input('The bill total is: $'))
+        if str(bill_total).endswith('0'):
+            billtotal = int(bill_total)
+        if bill_total > 300 or str(bill_total).endswith('7'):
+            bill_total -= bill_total * 0.03
+    except ValueError:
+        print('Invalid request')
+        return -1
     correct_value = False
     while not correct_value:
-        tip = int(input('Enter the tip. (10% - 12% - 15%): '))
-        if tip == 15 or tip == 12 or tip == 10:
-            correct_value = True
+        
+        try:
+            tip = int(input('Enter the tip. (10% - 12% - 15%): '))
+            if tip == 15 or tip == 12 or tip == 10:
+                correct_value = True
+            else:
+                print('Incorrect value, try again.')
+                break
+        except ValueError:
+            print('Please enter a number not any other type of character.')
     bill_total += bill_total * (tip / 100)
-    amount_of_people_paying = int(input('How many people are going to pay? '))
-    bill_total /= amount_of_people_paying
-    print(f'Each person is going to pay ${round(bill_total, 2)}')
+    
+    try:
+        amount_of_people_paying = int(input('How many people are going to pay? '))
+        bill_total /= amount_of_people_paying
+        print(f'Each person is going to pay ${round(bill_total, 2)}')
+    except ValueError:
+        print('Invalid request.')
+        return -1
 
 tip_calculator()
