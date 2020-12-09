2562ea0b42267bf2cd6d9fda3be3f0bf835a6424
Victor Kolis <victorkolis@protonmail.com>
2020-12-08 21:12:24 -0300

    20201208

---
+++ DerekBanas/01 - Variables/variables.py
@@ -0,0 +1,22 @@
+# Names used to store data in Python
+
+my_age = 26
+my_name = 'Victor Kolis'
+
+print('Hello', my_name)
+
+'''
+Multiline comment
+'''
+
+# Escape sequences
+
+str_1 = "\"This is a quote\""
+
+'''
+Newline: \n
+Backslash: \\
+Single Quote: \'
+Backspace: \b
+Tab: \t
+'''
