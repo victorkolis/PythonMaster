3b0a9548bc5cf819599f078d37bf8988c0714539
Victor Kolis <victorkolis@protonmail.com>
2020-12-09 21:40:46 -0300

    20201209

---
+++ DerekBanas/02 - user input and math functions/math_module_functions.py
@@ -0,0 +1,24 @@
+import math
+
+print("ceil(5.4) = ", math.ceil(5.4))
+print("floor(5.4) = ", math.floor(5.4))
+print("fabs(5.4) = ", math.fabs(-5.4))
+
+# Factorial = 1 * 2 * 3 * 4
+print("factorial(4) = ", math.factorial(4))
+
+# Return the remainder of a division
+print("factorial(4) = ", math.remainder(5, 4))
+
+# Receive a float and return an int
+print("trunc(4.418290) = ", math.trunc(4.418290))
+
+# Return x^y
+print("pow(4, 2) = ", math.pow(4, 2))
+
+# Return the square root
+print("sqrt(4) = ", math.sqrt(4))
+
+# Special values
+print("math.e = ", math.e)
+print("math.pi = ", math.pi)
