6b9ed5f08c92afa2afaf46a7c9fb596c2de93b50
Victor Kolis <victorkolis@protonmail.com>
2020-12-15 22:38:22 -0300

    20201215

--- OOP/FebinGeorge/Section 3 - Classes And Objects/main.py
+++ OOP/FebinGeorge/Section 3 - Classes And Objects/main.py
@@ -1,1 +1,10 @@
-# This is just a comment
+class Employee:
+    name = 'Victor'
+    designation = 'Sales Executive'
+    sales_made_this_week = 3
+
+    def has_achieved_target(self):
+        if self.sales_made_this_week >= 5:
+            print("Target has been achieved")
+        else:
+            print("Target has not been achieved")
