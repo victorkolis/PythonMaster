ae8203012eafaddbf213736c257b7bdda7a7d735
Victor Kolis <victorkolis@protonmail.com>
2020-12-17 16:35:01 -0300

    20201217

--- OneHundredDays/Day08/prime_number_checker.py
+++ OneHundredDays/Day08/prime_number_checker.py
@@ -1,23 +1,24 @@
 # prime_number_checker.py
 
-def prime(number):
-    isPrime = "?"
-    
-    number = abs(number)    
-    if number > 3 and number % 2 == 0  or number == 1:
-        return f"{number} is not a prime number."
-    elif number > 3:
-        for i in range(3, number):
-            if number % i == 0:
-                isPrime = f"{number} is not a prime number."
+def prime(number_entered):
+    is_prime = "?"
+
+    number_entered = abs(number_entered)
+    if number_entered > 3 and number_entered % 2 == 0 or number_entered == 1:
+        return f"{number_entered} is not a prime number."
+    elif number_entered > 3:
+        for i in range(3, number_entered):
+            if number_entered % i == 0:
+                is_prime = f"{number_entered} is not a prime number."
                 break
             else:
-                isPrime = f"{number} is a prime number."
+                is_prime = f"{number_entered} is a prime number."
     else:
-        isPrime = f"{number} is a prime number."
-        return isPrime
-    return isPrime    
+        is_prime = f"{number_entered} is a prime number."
+        return is_prime
+    return is_prime
+
 
 n = int(input("Check this number: "))
-number = prime(number = n)
+number = prime(number_entered=n)
 print(number)
