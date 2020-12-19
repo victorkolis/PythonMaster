4a186afc6f85a2798294e0a350b4e5095a6b7ed4
Victor Kolis <victorkolis@protonmail.com>
2020-12-18 22:24:31 -0300

    20201218

--- OneHundredDays/Day18/dashed_challenge.py
+++ OneHundredDays/Day18/dashed_challenge.py
@@ -1,5 +1,17 @@
 from turtle import Turtle
+from turtle import Screen
 
 bert = Turtle()
+screen = Screen()
 
-bert.
+
+def bert_walks():
+    for _ in range(10):
+        bert.forward(10)
+        bert.penup()
+        bert.forward(10)
+        bert.pendown()
+
+
+bert_walks()
+screen.exitonclick()
