9163d93c5c32f8f909ca27fcc7f19915e42a82d8
Victor Kolis <victorkolis@protonmail.com>
2020-12-25 21:05:10 -0300

    20201225

--- CODEWARS/HUMAN_READABLE_TIME/main.py
+++ CODEWARS/HUMAN_READABLE_TIME/main.py
@@ -1,9 +1,23 @@
 def make_readable(time_in_seconds):
-    seconds = '00'
-    minutes = '00'
-    hours = '00'
+    hours = 0
+    minutes = 0
+    seconds = time_in_seconds
 
-    pass
+    for second in range(time_in_seconds):
+        if seconds > 59:
+            seconds -= 60
+            minutes += 1
+            if minutes > 59:
+                minutes = 0
+                hours += 1
+    if hours < 10:
+        hours = f'0{str(hours)}'
+    if minutes < 10:
+        minutes = f'0{str(minutes)}'
+    if seconds < 10:
+        seconds = f'0{str(seconds)}'
+
+    return '{}:{}:{}'.format(hours, minutes, seconds)
 
 
-print(make_readable(1))
+print(make_readable(0))
