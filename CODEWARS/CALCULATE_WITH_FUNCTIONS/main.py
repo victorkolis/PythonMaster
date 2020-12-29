18dca7183ae8cd45a4b07d0e07425e861a6cbaf9
Victor Kolis <victorkolis@protonmail.com>
2020-12-29 16:30:23 -0300

    20201229

--- CODEWARS/CALCULATE_WITH_FUNCTIONS/main.py
+++ CODEWARS/CALCULATE_WITH_FUNCTIONS/main.py
@@ -1,19 +1,167 @@
-def zero(*args): return 0
-def one(*args): return 1
-def two(*args): return 2
-def three(*args): return 3
-def four(*args): return 4
-def five(*args): return 5
-def six(*args): return 6
-def seven(*args): return 7
-def eight(*args): return 8
-def nine(*args): return 9
+def zero(*args):
+    number = 0
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
 
 
-def plus(arg): return '+'
-def minus(arg): return '-'
-def times(arg): return '*'
-def divided_by(arg): return ''
+def one(*args):
+    number = 1
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
 
 
-print(zero(plus(one())))
+def two(*args):
+    number = 2
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def three(*args):
+    number = 3
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def four(*args):
+    number = 4
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def five(*args):
+    number = 5
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def six(*args):
+    number = 6
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def seven(*args):
+    number = 7
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def eight(*args):
+    number = 8
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def nine(*args):
+    number = 9
+    if len(args) == 0:
+        return number
+    elif len(args) == 1:
+        for character in args:
+            if '+' in character:
+                return args[0][0] + number
+            elif '-' in character:
+                return number - args[0][0]
+            elif '*' in character:
+                return args[0][0] * number
+            elif '/' in character:
+                return number // args[0][0]
+
+
+def plus(number): return number, '+'
+def minus(number): return number, '-'
+def times(number): return number, '*'
+def divided_by(number): return number, '/'
+
+
+print(eight(minus(three())))
