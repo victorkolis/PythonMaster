80ffab203d1009c158401cb2b15ccbf7e6720ecf
Victor Kolis <victorkolis@protonmail.com>
2021-01-03 21:24:17 -0300

    20210103

--- OneHundredDays/Day20/main.py
+++ OneHundredDays/Day20/main.py
@@ -2,6 +2,7 @@ from turtle import Screen
 from snake import Snake
 import time
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor('black')
@@ -9,12 +10,18 @@ screen.title('Snake')
 screen.tracer(0)
 
 snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, 'Up')
+screen.onkey(snake.down, 'Down')
+screen.onkey(snake.left, 'Left')
+screen.onkey(snake.right, 'Right')
+
+
 game_is_on = True
-counter = 0
 while game_is_on:
-    counter += 1
     screen.update()
     time.sleep(0.1)
     snake.move()
-    if counter == 20:
-        break
+
+screen.exitonclick()
