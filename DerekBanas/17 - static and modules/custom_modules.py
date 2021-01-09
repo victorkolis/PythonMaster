de7d82b7c35f673da1579c7923292d4855fb0de1
Victor Kolis <victorkolis@protonmail.com>
2021-01-09 18:29:20 -0300

    20210109

---
+++ DerekBanas/17 - static and modules/custom_modules.py
@@ -0,0 +1,5 @@
+def get_addition(*args):
+    addition = 0
+    for number in args:
+        addition += number
+    return addition
---
+++ DerekBanas/17 - static and modules/using_custom_modules.py
@@ -0,0 +1,3 @@
+from custom_modules import *
+
+print('The addition of all the element is {}'.format(get_addition(1, 2, 3, 4)))
