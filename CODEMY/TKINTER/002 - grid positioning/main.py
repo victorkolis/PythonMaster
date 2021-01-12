d9a03db6bb5866dbcadd82568563adaf565d6086
Victor Kolis <victorkolis@protonmail.com>
2021-01-11 23:34:11 -0300

    20210111

--- CODEMY/TKINTER/002 - grid positioning/main.py
+++ CODEMY/TKINTER/002 - grid positioning/main.py
@@ -7,6 +7,7 @@ root = Tk()
 label_1 = Label(root, text='LEARNING THE GRID SYSTEM')
 label_2 = Label(root, text='IN TKINTER')
 
+# Grid positioning is object related
 label_1.grid(row=0, column=0)
 label_2.grid(row=1, column=1)
 
