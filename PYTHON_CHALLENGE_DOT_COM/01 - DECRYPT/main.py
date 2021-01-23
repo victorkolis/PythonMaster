bb486111d218611b6fb65e6baaea9942ef385ded
Victor Kolis <victorkolis@protonmail.com>
2021-01-23 14:55:45 -0300

    20210122

---
+++ PYTHON_CHALLENGE_DOT_COM/01 - DECRYPT/main.py
@@ -0,0 +1,5 @@
+text = """g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb 
+rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."""
+
+new_text = text.replace('m', 'k').replace('q', 'n').replace('g', 'a')
+print(new_text)
