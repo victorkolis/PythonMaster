1cdf7737248f36be55ed5b0828924a33c97417a8
Victor Kolis <victorkolis@protonmail.com>
2021-01-29 14:00:06 -0300

    20210129

--- REGEX/LIST_OF_CHARACTERS/main.py
+++ REGEX/LIST_OF_CHARACTERS/main.py
@@ -7,7 +7,22 @@ I am victor
 i aM Victor
 i Am VICtor
 i AM vICTOR
+i am aictor
+He is wictor
+they are mictor
+It's viktor
 '''
+
 
 print(re.findall(r'[Vv]ictor', text))
 print(re.findall(r'[Vv][Ii][Cc][Tt][Oo][Rr]', text))
+
+# Range
+print(re.findall(r'[a-v]ictor', text))
+
+print(re.findall(r'[a-zA-Z0-9]ictor', text))
+
+
+# Flags, re.I = re.IGNORECASE
+
+print(re.findall(r'victor', text, flags=re.IGNORECASE))
