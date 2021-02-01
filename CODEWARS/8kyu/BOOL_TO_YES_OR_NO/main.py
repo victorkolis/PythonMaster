f0cd8bf41f02b94f9adda7533a4f4696afb784b5
Victor Kolis <victorkolis@protonmail.com>
2021-02-01 08:18:13 -0300

    20210201

---
+++ CODEWARS/8kyu/BOOL_TO_YES_OR_NO/main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Convert boolean values to strings 'Yes' or 'No'.
+# False -> 'No', True -> 'Yes'
+
+
+def bool_to_word(boolean):
+	return 'Yes' if boolean else 'No'
+	
+
+print(bool_to_word(True))
