780e2e9e80c00f9255d1281dc5b455bec836f2e1
Victor Kolis <victorkolis@protonmail.com>
2021-02-01 23:59:30 -0300

    20210201

--- CODEWARS/8kyu/GROWTH_OF_A_POPULATION/main.py
+++ CODEWARS/8kyu/GROWTH_OF_A_POPULATION/main.py
@@ -1,7 +1,3 @@
 #!/usr/bin/env python3
 
-def nb_year(wanted_population, percentage, ):
-	# https://www.codewars.com/kata/563b662a59afc2b5120000c6/train/python
-	pass
-	
-	# NOT YET FINISHED.
+# NOT YET FINISHED
