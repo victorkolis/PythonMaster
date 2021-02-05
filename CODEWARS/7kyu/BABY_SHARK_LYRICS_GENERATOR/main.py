80280f4a49224a9a77a8ef6a6c83b744a30107fd
Victor Kolis <victorkolis@protonmail.com>
2021-02-05 07:46:08 -0300

    20210205

---
+++ CODEWARS/7kyu/BABY_SHARK_LYRICS_GENERATOR/main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+def baby_shark_lyrics():
+	p = ('Baby', 'Mommy', 'Daddy', 'Grandma', 'Grandpa')
+	a = 'Let\'s go hunt'
+	d = ' doo' * 6
+	
+	l = ''
+	for n in p:
+		l += f'{n} shark,{d}\n' * 3
+		l += f'{n} shark!\n'
+	
+	l += f'{a},{d}\n' * 3
+	l += f'{a}!\nRun away,…'
+	return l
+	
+
+print(baby_shark_lyrics())
+
+"""
+Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark, doo doo doo doo doo doo
+Baby shark!
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark, doo doo doo doo doo doo
+Mommy shark!
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark, doo doo doo doo doo doo
+Daddy shark!
+Grandma shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark, doo doo doo doo doo doo
+Grandma shark!
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark, doo doo doo doo doo doo
+Grandpa shark!
+Let's go hunt, doo doo doo doo doo doo
+Let's go hunt, doo doo doo doo doo doo
+Let's go hunt, doo doo doo doo doo doo
+Let's go hunt!
+Run away,…
+"""
