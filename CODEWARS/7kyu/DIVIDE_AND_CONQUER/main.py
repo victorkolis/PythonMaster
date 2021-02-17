5c03039103087b54f0a9b9541ccc6d076f9036c9
Victor Kolis <victorkolis@protonmail.com>
2021-02-17 00:21:52 -0300

    20210217

--- CODEWARS/7kyu/DIVIDE_AND_CONQUER/main.py
+++ CODEWARS/7kyu/DIVIDE_AND_CONQUER/main.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 
+# Add the ints and subtract by the strings added up
+
 def div_con(x):
-	string_to_int = ''
-	int_numbers = 0
 	
+	subtracted = 0
+	subtractor = 0
 	
+	for element in x:
+		try:
+			if element.isnumeric():
+				subtractor += int(element)
+		except AttributeError:
+			subtracted += int(element)
+			
+	return subtracted - subtractor
+	
+
+print(div_con([9, 3, '7', '3']))
