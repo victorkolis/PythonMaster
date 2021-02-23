6480a233f8aaf65f78da607ab7d5e85f71e8a7f7
Victor Kolis <victorkolis@protonmail.com>
2021-02-22 23:34:17 -0300

    20210222

---
+++ CODEWARS/7kyu/SORT_BY_LAST_CHAR/main*.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Given a string, return a last-letter sorted array of words
+
+def last(s):
+	#
+	
+	return s
+
+
+print(last('what time are we climbing up the volcano'))
