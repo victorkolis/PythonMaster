5238a5703ff923596df595f809eec948dcb6a0b8
Victor Kolis <victorkolis@protonmail.com>
2021-03-01 13:17:18 -0300

    20210301

--- CODEWARS/7kyu/SEPARATE_BASIC_TYPES/main.py
+++ CODEWARS/7kyu/SEPARATE_BASIC_TYPES/main.py
@@ -1,3 +1,17 @@
 #!/usr/bin/env python3
 
-def 
+# Give an array such: ['a', 1, 2, False, 'b'] ->
+"""
+{
+	int:  [1, 2]
+	str:  ['a', 'b']
+	bool: [False]
+}
+"""
+
+def separate_types(seq):
+	for element in seq:
+		pass
+	
+
+print(separate_types([1, 2, 'a', 'b', False]))
